6f8b2d14d94d213c38616e23c07e3359d0e382cd
Oskar Lövgren <oskarlov@kth.se>
2023-11-24 11:56:22 +0100

    api

---
+++ apiConfig.js
@@ -0,0 +1,7 @@
+const BASE_URL="rapidapi.com";   // the DH2642 proxy server
+const API_KEY="3f784bebf2msha358f0c92b3b550p16614ajsn50402fe473bd";
+
+export {BASE_URL, API_KEY}
+// then in other files you can write import {BASE_URL, API_KEY} from /src/apiConfig.js;
+//cool
+

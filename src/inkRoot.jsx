c37df522eecbe1ef41dbd3bdcf1ada6acccf110c
Oskar Lövgren <oskarlov@kth.se>
2023-11-29 13:38:07 +0100

    inkRoot 1.2

--- src/inkRoot.jsx
+++ src/inkRoot.jsx
@@ -12,7 +12,7 @@ function inkRoot(props){
     return (  
     <div class="homepage">
 
-        <HomePage class="center"/>
+        <HomePage/>
          {/*<Movie/>
         <SearchResult/>
         <User/>

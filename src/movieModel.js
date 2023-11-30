1022b526313513f6add21b7040b20b2dc4117a29
Stefan Ivchenko <sivc@kth.se>
2023-11-30 11:52:00 +0100

    Movie Model updater

--- src/movieModel.js
+++ src/movieModel.js
@@ -1,14 +1,26 @@
 import {fetchMovieData} from "/src/movieSource.js"
 import resolvePromise from "./resolvePromise";
 export default {
-    movies: [], 
+    allMovies: [], 
+    favouriteMovies:[],
     currentmovie: null,
     getResultsPromiseState: {},
     searchname: null,
+
     
 
 
     getData() {
         return resolvePromise( fetchMovieData(), this.getResultsPromiseState) // returns a promise
-    }
+    },
+    addToFavourites(movie){
+        this.favouriteMovies= [...this.favouriteMovies, movie];
+    },
+
+    removeFromFaves(movie){
+        function shouldWeKeepDishCB(movieSearched){return movieSearched.i !== movie.i
+        }
+        this.favouriteMovies= this.favouriteMovies.filter(shouldWeKeepDishCB);
+    },
+
 };

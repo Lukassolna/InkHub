42fc553af4175d9e59134f5cbf65cc5628891deb
NiklasDahlbom <ndahlbom@kth.se>
2023-11-30 11:21:59 +0100

    poster made

--- src/views/writersView.jsx
+++ src/views/writersView.jsx
@@ -3,15 +3,32 @@ function WritersView(props){
 
     return ( 
     <div class="debug">
-        <tr>this is the writerspage
-            <button  onClick={props.onButtonClick}>i like slurping on some OJ</button>
-            <button  onClick={props.onButtonClick}>i like slurping on some OJ</button>
-            <button  onClick={props.onButtonClick}>i like slurping on some OJ</button>
-        </tr>
+        <table class="center">
+            <tr>
+                <td class="headers">this is the writerspage</td>
 
-        <tr>
-            <img src={"https://thispersondoesnotexist.com/"} alt="Random person" height="300"></img>
-        </tr>
+                <td>
+                <button  class="oval-button" onClick={props.onButtonClick}>Overveiw</button>
+                <button  class="oval-button" onClick={props.onButtonClick}>Movies</button>
+                <button  class="oval-button" onClick={props.onButtonClick}>Regular actors</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                <img src={"https://thispersondoesnotexist.com/"} alt="Random person" height="300"></img>
+                </td>
+
+                <td>
+                <div class="poster-container">
+                    <img class="poster" src={"https://thispersondoesnotexist.com/"} alt="Poster 1"  ></img>
+                    <img class="poster" src={"https://thispersondoesnotexist.com/"} alt="Poster 2"  ></img>
+                    <img class="poster" src={"https://thispersondoesnotexist.com/"} alt="Poster 3"  ></img>
+                    <img class="poster" src={"https://thispersondoesnotexist.com/"} alt="Poster 4"  ></img>
+                </div>
+                </td>
+            </tr>
+        </table>
     </div>
     )
 }

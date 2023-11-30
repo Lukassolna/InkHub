010d5281af1a7d0e7aaf88ffab3c463b233c98a5
lukasja <lukasja@kth.se>
2023-11-30 13:00:41 +0100

    new api

--- src/movieModel.js
+++ src/movieModel.js
@@ -1,14 +1,15 @@
 import {fetchMovieData} from "/src/movieSource.js"
+import {fetchPopularMovies} from "/src/IMDBsource.js"
 import resolvePromise from "./resolvePromise";
 export default {
     movies: [], 
     currentmovie: null,
     getResultsPromiseState: {},
     searchname: null,
-    
+
 
 
     getData() {
-        return resolvePromise( fetchMovieData(), this.getResultsPromiseState) // returns a promise
+        return resolvePromise( fetchPopularMovies(), this.getResultsPromiseState) // returns a promise
     }
 };

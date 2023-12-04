a92167c4a9cf856242ef64c583b913ac6ec76ac8
NiklasDahlbom <ndahlbom@kth.se>
2023-12-04 15:48:03 +0100

    render function added

--- src/inkRoot.jsx
+++ src/inkRoot.jsx
@@ -12,7 +12,7 @@ function inkRoot(props){
     return (  
     <div class="homepage">
 
-        <Writers model = {props.model}/>
+        <HomePage/>
          {/*<Movie/>
         <SearchResult/>
         <User/>
@@ -45,3 +45,27 @@ function makeRouter(props){
   }
   
   export {makeRouter}
+
+/*
+const = VueRoot={'
+  setup(){
+    
+    return function renderACB(){
+      if(myModel.user === undefiend)
+        return <img src="https://brfenergi.se/iprog/loading.gif"></img>
+      
+      if(myModel.user === null)
+        return <Auth auth={auth}/>;
+
+      return (
+        "!"myModel.ready && <img src="https://brfenergi.se/iprog/loading.gif"></img>
+        <div class="homepage" >
+          <HomePage>
+        </div>
+
+      )
+    }
+  }
+
+}
+  */
